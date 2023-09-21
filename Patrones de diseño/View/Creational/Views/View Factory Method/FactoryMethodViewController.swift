ba05a96d3c0bf360080fb6266c26a3e2fabcb69a
Luis Humberto <beto020017@gmail.com>
2023-09-21 16:11:20 -0600

    Navegación creaciones

--- Patrones de diseño/View/Creational/Views/View Factory Method/FactoryMethodViewController.swift
+++ Patrones de diseño/View/Creational/Views/View Factory Method/FactoryMethodViewController.swift
@@ -11,6 +11,7 @@ class FactoryMethodViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
     
